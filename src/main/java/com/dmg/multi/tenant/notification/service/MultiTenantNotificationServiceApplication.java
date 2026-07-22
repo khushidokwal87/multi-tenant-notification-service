9979cc183343f5805f397334483a80ca3a1ec305
Khushi Dokwal <khushidokwal@Khushis-MacBook-Air.local>
2026-07-22 19:28:39 +0530

    Initial Spring Boot Project Set up

---
+++ src/main/java/com/dmg/multi/tenant/notification/service/MultiTenantNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmg.multi.tenant.notification.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmg/multi/tenant/notification/service/MultiTenantNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmg.multi.tenant.notification.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
